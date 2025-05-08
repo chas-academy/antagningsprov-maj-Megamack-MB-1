bdff7bd2cad6b1f6b48ade0a748ffe5d9a3482b1
Megamack-MB <mackanb99@gmail.com>
2025-05-08 18:22:54 +0200

    Update uppg7.js
    Deklarerade const variablerna a och b samt tilldelade dessa talen 5 och 10. Skapade en funktion som läser in talen, adderar dessa och returnerar summan.

--- uppg7.js
+++ uppg7.js
@@ -1,12 +1,19 @@
-
+uppg7();
 
 function uppg7() {
    
     // skapa en funktion som tar in två tal som argument
     // funktionen ska returnera summan av talen
     // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
+const a = 5;
+const b = 10;
+operation(a, b);
+console.log(sum);
+}
 
- 
+function operation(a, b) {
+sum = a+b;
+return sum;
 }
 
 module.exports = { uppg7 };
