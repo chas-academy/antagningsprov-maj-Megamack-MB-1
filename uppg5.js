28150969839e09b41dea9a52f2292d2d67fe36e1
Megamack-MB <mackanb99@gmail.com>
2025-05-08 16:52:40 +0200

    Update uppg5.js
    Lade till ett metodanrop i början. I metoden deklarerade jag 2 variabler, a och b, och tilldelade dessa två heltal. Slutligen lade jag till en if-else sats som jämför vilket tal som är störst, a eller b, som sedan skriver ut i text vilken som är störst. let gör att det går att ändra värden i a och b för att få olika resultat.

--- uppg5.js
+++ uppg5.js
@@ -1,9 +1,16 @@
+uppg5();
 
 function uppg5() {
     // skriv en if-sats som jämför två tal
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
-
+let a = 5;
+let b = 10;
+if (a>b){
+    console.log("Det första talet är större än det andra");
+} else {
+    console.log("Det andra talet är större än det första")
+}
 
 
 }
