66d82dc9cb8104af3f04dc101cc5e4d97795f33c
Megamack-MB <mackanb99@gmail.com>
2025-05-08 15:04:32 +0200

    Update uppg2.js
    Deklarerade variabeln name med en sträng, och lade till console.log som sedan skriver ut namnet vid anrop av metoden uppg2.

--- uppg2.js
+++ uppg2.js
@@ -2,8 +2,12 @@
 function uppg2() {
    // Skriv din kod här
    // deklarera en variable med ditt namn och skriv ut det i konsolen
+ let name = "Marcus"
 
+ console.log(name);
 
 }
+
+uppg2();
 
 module.exports = { uppg2 };
