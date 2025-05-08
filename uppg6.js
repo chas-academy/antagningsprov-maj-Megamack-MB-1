6a80058096afda547d462a095c1fa653bbf17dc8
Megamack-MB <mackanb99@gmail.com>
2025-05-08 17:50:23 +0200

    Update uppg6.js
    Lade till metodanrop i början. I metoden skapade jag en lista med namn. Med en for-loop och console.log kommer alla nuvarande namn i listan att skrivas ut i terminalen.

--- uppg6.js
+++ uppg6.js
@@ -1,11 +1,21 @@
-
+uppg6();
 
 function uppg6(){
 
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
+  let listofNames = [
+        {name: "Adam"},
+        {name: "Bertil"}, 
+        {name: "Carl"}, 
+        {name: "David"},
+        {name: "Erik"}
+    ]
+    
+    for (person of listofNames){
+        console.log(person.name)
+    }
 
- 
 }
 
 module.exports = { uppg6 };
