10eceb599e7842065dced5a125e7c2b767956ef4
Megamack-MB <mackanb99@gmail.com>
2025-05-09 11:51:29 +0200

    Update uppg9.js
    Lade till en funktion som kollar om talet är jämnt delbart eller inte. Om det inte blir rest av modulus operationen räknas det som jämnt, annars udda.

--- uppg9.js
+++ uppg9.js
@@ -1,4 +1,4 @@
-
+uppg9();
 
 function uppg9 (){
     // skapa en funktion som heter sort och tar in en array av siffror som argument
@@ -7,8 +7,23 @@ function uppg9 (){
     // resultatet ska loggas i formatet: talet och sen ordet "jämt" eller "udda"
 
     // anropa funktionen och skicka med en array som argument
-
+    
+let listofNumbers = [1, 2, 3, 4, 5]
+    
+sort(listofNumbers);
   
+}
+
+function sort(listofNumbers){
+for (i = 0; i < listofNumbers.length; i++) {
+    if (!listofNumbers % [i] === 0) {
+        console.log(listofNumbers[i]+" udda");
+    }
+    else {
+        console.log(listofNumbers[i]+" jämnt");
+    }
+}
+ 
 }
 
 module.exports = { uppg9 };
