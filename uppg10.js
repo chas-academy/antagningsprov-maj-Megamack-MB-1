ff3f538e6d28fef1a4a92f85d5ec42ae38a03011
Megamack-MB <mackanb99@gmail.com>
2025-05-09 12:36:43 +0200

    Update uppg10.js
    Lade till en funktion som loopar igenom array med numbers och sorterar talen i den utefter uppgiftens specifikation. Efteråt läggs talen till i de 3 olika listorna som skapats.

--- uppg10.js
+++ uppg10.js
@@ -1,3 +1,5 @@
+uppg10();
+
 function uppg10(){
     const numbers = [5, 12, 7, 10, 20, 9, 11];
   
@@ -7,11 +9,37 @@ function uppg10(){
     // smallOdd ska innehålla tal mindre än 10 och udda
     // other ska innehålla alla andra tal
     // returnera ett objekt med de tre arrayerna
+let bigEven = [];
+let smallOdd =[];
+let other =[];
 
-  
-    
-  
+sort(numbers, bigEven, smallOdd, other)
+
+console.log(sortList);
+
   }
+
+function sort(numbers, bigEven, smallOdd, other){
+    for (i = 0; i < numbers.length; i++)
+    {
+        if (number[i] > 10 && number % [i] === 0){
+            bigEven.add(number[i])
+        }
+        else if (number[i] < 10 && !number % [i] === 0)
+        {
+            smallOdd.add(number[i])
+        }
+        else {
+            other.add(number[i])
+        }
+    }
+    let sortList = {
+        bigEven : [numbers],
+        smallOdd : [numbers],
+        other : [numbers]
+    }
+    return sortList;
+}
   
   module.exports = { uppg10 };
   
